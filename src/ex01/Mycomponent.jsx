b6767333418cfac8c8af36705435268f5b997e82
misoyblue <misoyblue@gmail.com>
2025-12-03 13:18:00 +0900

    first commit

---
+++ src/ex01/Mycomponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Mycomponent = ({name, children}) => {
+
+    return (
+        <div>
+        안녕하세요 제이름은 {name}입니다 <br />
+        children 값은 {children}입니다.
+        </div>
+    );
+};
+
+export default Mycomponent;
---
+++ src/ex01/State.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const state = () => {
+    return (
+        <div>
+           sodidifso
+        </div>
+    );
+};
+
+export default state;
